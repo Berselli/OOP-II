c6dad66eaae8fe84363a477022f4490c0224b642
Juliano Berselli <Berselli@users.noreply.github.com>
2018-07-01 15:52:51 -0300

    List 6 Number 2 code update

--- List6/Number2/FileRW.java
+++ List6/Number2/FileRW.java
@@ -14,18 +14,14 @@ import java.util.ArrayList;
 public class FileRW {
     
   public static String readFile(String filePath) {
-    // podia ser o caminho: "c:\\Pasta\\arquivo.txt"
     ArrayList<String> array = new ArrayList();
-    //filePath = "temp.txt";
     String fullText = "";
     try{
-      // File tem vários métodos úteis
       File file_1 = new File(filePath);
       
       RandomAccessFile raf = new RandomAccessFile(file_1, "rw");
       String line = null;
       while((line = raf.readLine()) != null) {
-          //System.out.println(linha);
           array.add(line);
       }
       
@@ -35,10 +31,8 @@ public class FileRW {
       fullText = array.stream().map((item) -> item + "\n").reduce(fullText, String::concat);
       
     } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
     return fullText;
@@ -46,26 +40,17 @@ public class FileRW {
   
   public static void writeFile(String filePath, String fullText)
   {
-     //String nomeArquivo = "temp.txt";
     try{
-      // File tem vários métodos úteis
       File arquivo = new File(filePath);
       
       RandomAccessFile raf = new RandomAccessFile(arquivo, "rw");
       
       raf.setLength(0);
       
-      //raf.write(fullText.getBytes());
-      
       for(String line : fullText.split("\n")){
           raf.write((line + "\r\n").getBytes());
       }
-              /*
-      for(String ln : array)
-      {
-          raf.write((ln + "\r\n").getBytes());
-      }
-      */
+      
       raf.close();
     } catch (FileNotFoundException e) {
       // TODO Auto-generated catch block
@@ -75,29 +60,4 @@ public class FileRW {
       e.printStackTrace();
     } 
   }
-  
-  public static String newFile(){
-      String filePath = "temp.txt";
-      String fullText = "";
-      
-      try{
-      // File tem vários métodos úteis
-      File file_1 = new File(filePath);
-      
-      RandomAccessFile raf = new RandomAccessFile(file_1, "rw");
-      
-      raf.setLength(0);
-      
-      raf.close();
-      
-    } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return fullText;
-  }
-
 }
